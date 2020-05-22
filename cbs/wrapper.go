2786924a342c123a1d85733d436de2b7135b745a
kali kaneko <kalikaneko@riseup.net>
2020-05-22 19:19:47 +0200

    add mutex

--- cbs/wrapper.go
+++ cbs/wrapper.go
@@ -5,6 +5,7 @@ package main
 import (
 	"fmt"
 	"reflect"
+	"sync"
 	"unsafe"
 )
 
@@ -19,18 +20,12 @@ type Hooks struct {
 	OnThingB string
 }
 
+var mut sync.Mutex
 var cbs = make(map[string](*[0]byte))
 
-func fireCb(name string) {
-	cb := cbs[name]
-	if cb != nil {
-		C._do_callback(cb)
-	} else {
-		fmt.Println("ERROR: callback not set", name)
-	}
-}
-
 func registerCb(name string, fp unsafe.Pointer) {
+	mut.Lock()
+	defer mut.Unlock()
 	h := &Hooks{}
 	v := reflect.Indirect(reflect.ValueOf(&h))
 	hf := v.Elem().FieldByName(name)
@@ -38,6 +33,17 @@ func registerCb(name string, fp unsafe.Pointer) {
 		fmt.Println("ERROR: not a valid hook:", name)
 	} else {
 		cbs[name] = (*[0]byte)(fp)
+	}
+}
+
+func fireCb(name string) {
+	mut.Lock()
+	defer mut.Unlock()
+	cb := cbs[name]
+	if cb != nil {
+		C._do_callback(cb)
+	} else {
+		fmt.Println("ERROR: callback not set", name)
 	}
 }
 
